0b76a7320a3928a09c033d1aa7a65bd396c3460f
Maometto <momo2online@gmail.com>
2022-09-15 17:05:02 +0200

    files added

---
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -0,0 +1,21 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly
+// Generated on Thu, 15 Sep 2022 14:42:58 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryColor {
+    public static let baseGrayDark = UIColor(red: 0.333, green: 0.333, blue: 0.333, alpha: 1)
+    public static let baseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let baseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    public static let baseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let baseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let fontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let fontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let fontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+}
---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Thu, 15 Sep 2022 14:42:58 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
